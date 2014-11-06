6f96a22715d30c147d78bd552f4938a8d8dd3039
Richard Sailer <richard@weltraumpflege.org>
2014-11-06 23:17:19 +0100

    enabled auto closing of edit window after saving

--- Qooxdoo.pm
+++ Qooxdoo.pm
@@ -8,7 +8,8 @@ use POE;
 
 our @ISA;
 
-sub new {
+sub new
+{
    my $proto = shift;
    my $class = ref($proto) || $proto;
    my $self = shift;
@@ -17,6 +18,37 @@ sub new {
    $self = $self ? $self : {};
    bless ($self, $class);
    return $self;
+}
+
+
+sub onSaveEditEntry
+{
+    my $self       = shift;
+    my $options    = shift;
+    my $moreparams = shift;
+
+    unless ( ( !$moreparams )
+        && $options->{curSession}
+        && $options->{table}
+        && $options->{"q"}
+        && $options->{oid}
+        && $options->{connection} )
+    {
+        Log(
+            "onSaveEditEntry: Missing parameters: table:"
+              . $options->{table}
+              . ":curSession:"
+              . $options->{curSession} . ": !",
+            $ERROR
+        );
+        return undef;
+    }
+
+    # enable autoclosing of the window when save button is preesed
+    $options->{close}++;
+
+    my $return_value = $self->SUPER::onSaveEditEntry( $options, $moreparams );
+    return $return_value;
 }
 
 1;
