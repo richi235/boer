dfbac64417d00111a42862f1ffca87967fd9fc17
Richard Sailer <richard@weltraumpflege.org>
2014-11-07 01:28:18 +0100

    reformated DBManager.pm with perltidy

--- DBManager.pm
+++ DBManager.pm
@@ -8,14 +8,14 @@ use ADBGUI::Tools qw(Log);
 our @ISA;
 
 sub new {
-   my $proto = shift;
-   my $class = ref($proto) || $proto;
-   my $self = shift;
-   my $parent = $self ? ref($self) : "";
-   @ISA = ($parent) if $parent;
-   $self = $self ? $self : {};
-   bless ($self, $class);
-   return $self;
+    my $proto  = shift;
+    my $class  = ref($proto) || $proto;
+    my $self   = shift;
+    my $parent = $self ? ref($self) : "";
+    @ISA = ($parent) if $parent;
+    $self = $self ? $self : {};
+    bless( $self, $class );
+    return $self;
 }
 
 1;
